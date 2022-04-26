f62b723c203d3fc5e9b4ede90150b5216f3452e9
Brianna Heggeseth <bcheggeseth@gmail.com>
2022-04-26 12:34:34 -0500

    Brianna's exploration

--- Spatial.R
+++ Spatial.R
@@ -52,8 +52,8 @@ names(race) = c('GEOID','NAME',trimws(paste0('Race_',str_sub(NAMES,0,26))))
 DC_data <- DC_quant %>% left_join(race)
 
 
-
-colleges <- read_csv("D:\\STAT 452\\Correlated-Capstone\\hd2020.csv")
+colleges <- read_csv("hd2020.csv")
+#colleges <- read_csv("D:\\STAT 452\\Correlated-Capstone\\hd2020.csv")
 colleges <- sf::st_as_sf(colleges,coords = c('LONGITUD','LATITUDE'))
 st_crs(colleges) <- DC_data %>% st_crs()
 
@@ -64,23 +64,25 @@ colleges_sub <- colleges[col_coord$X > st_bbox(DC_data)$xmin &  col_coord$X < st
 #Approx 1/2 mile radius
 DC_data$NumColleges <- st_intersects(DC_data,st_buffer(colleges_sub,dist=800)) %>% lengths()
 
-areawater <- read_sf("D:\\STAT 452\\Correlated-Capstone\\Waterbodies") 
-areawater <- st_transform(areawater, 6488)
-roads <- read_sf("D:\\STAT 452\\Correlated-Capstone\\tl_2018_11001_roads")
+areawater <- read_sf("Waterbodies") 
+#areawater <- read_sf("D:\\STAT 452\\Correlated-Capstone\\Waterbodies") 
+areawater <- st_transform(areawater,crs = st_crs(DC_data))
+roads <- read_sf("tl_2018_11001_roads")
+#roads <- read_sf("D:\\STAT 452\\Correlated-Capstone\\tl_2018_11001_roads")
 roads <- st_transform(roads,crs = st_crs(DC_data))
-roads_sub <- roads %>% filter( (st_intersects(roads,DC_data) %>% lengths()) > 0)
+roads_sub <- roads %>% filter( (st_intersects(roads,DC_data) %>% lengths()) > 0) %>% filter(RTTYP %in% c('U','I'))
+roads_sub <- st_crop(roads_sub,st_bbox(DC_data))
+DC_data$AREA = st_area(DC_data) %>% as.vector() #square feet
 
 # library(crsuggest)
 # suggest_crs(DC_data)
+#areawater <- st_transform(areawater, 6488)
+#colleges_sub <- st_transform(colleges_sub,crs = 6488)
+#DC_data <- st_transform(DC_data,crs = 6488)
+#roads_sub <- st_transform(roads_sub,crs=6488) 
 
-colleges_sub <- st_transform(colleges_sub,crs = 6488)
-DC_data <- st_transform(DC_data,crs = 6488)
-DC_data$AREA = st_area(DC_data) %>% as.vector()
-roads_sub <- st_transform(roads_sub,crs=6488) %>% filter(RTTYP %in% c('U','I'))
-roads_sub <- st_crop(roads_sub,st_bbox(DC_data))
-
+areawater <- st_make_valid(areawater)
 DC_data$DistToRiver = st_distance(DC_data,areawater %>% filter(GIS_ID == 'WaterPly_11')) %>% as.vector()
-
 distToRoads <- st_distance(DC_data,roads_sub) %>% units::drop_units() %>% as.matrix() 
 
 DC_data$MinDistToHwy = distToRoads %>% apply(1,min)
